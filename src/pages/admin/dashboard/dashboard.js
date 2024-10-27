8b7a484f359de5411f13d26626cf3f00f7317329
michael-montgomery <thehealthyprogrammer@outlook.com>
2024-10-27 00:26:30 -0400

    additional styling to admin pages.

--- src/pages/admin/dashboard/dashboard.js
+++ src/pages/admin/dashboard/dashboard.js
@@ -1,17 +1,38 @@
 import AdminMenu from '../../../components/admin/menu/menu';
 import Header from '../../../components/header/header';
+import users from '../../../data/users';
 import './dashboard.css';
 
 export default function AdminDashboard() {
+
+function calculatePercentOf(smallNum, largeNum) {
+    return (smallNum / largeNum) * 100
+}
+
+
     return (
         <>
-        <Header></Header>
-        <div className='admin-page-wrapper'>
-            <AdminMenu></AdminMenu>
-            <div className='admin-right-content-wrapper'>
-            <h2>Admin Dashboard</h2>
+            <Header></Header>
+            <div className='admin-page-wrapper'>
+                <AdminMenu></AdminMenu>
+                <div className='admin-right-content-wrapper'>
+                    <h2>Admin Dashboard</h2>
+                    <div className='inner-container'>
+                        <div className='widget' id='resident-count-widget'><p className='widget-number'>{users.length}</p><p className='widget-text'>Residents</p></div>
+                        <div className='widget'><p className='widget-number'>33%</p><p className='widget-text'>Delinquency</p></div>
+                        <div className='widget'></div>
+                        <div className='widget'></div>
+                        
+                    </div>
+                    <div className='inner-container'>
+                        <div className='widget'></div>
+                        <div className='widget'></div>
+                        <div className='widget'></div>
+                        <div className='widget'></div>
+                        
+                    </div>
+                </div>
             </div>
-        </div>
         </>
     )
 }
--- src/pages/admin/users/users.js
+++ src/pages/admin/users/users.js
@@ -4,6 +4,8 @@ import DeliquentPill from '../../../components/deliquent-pill/deliquentPill';
 import Header from '../../../components/header/header';
 import users from '../../../data/users';
 import './users.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 export default function AdminUsers() {
     return (
@@ -13,7 +15,7 @@ export default function AdminUsers() {
                 <AdminMenu></AdminMenu>
                 <div className='admin-right-content-wrapper'>
                     {/* <h2>Admin User Settings</h2> */}
-                    <h2>Users <span id='record-count-span'>{users.length}</span></h2>
+                    <h2 id='heading'>Users <span id='record-count-span'>{users.length}</span></h2>
                     <ul id='admin-users-list'>
                         {
                             users.map((user) => {
@@ -23,7 +25,7 @@ export default function AdminUsers() {
                                             <p className='users-list-name'>{`${user.firstName} ${user.lastName}`}</p>
                                             <p className='users-list-address'>{`${user.addressLine1}`}</p>
                                             <DeliquentPill user={user}></DeliquentPill>
-                                            <Link to={`/admin-view-user/${user.id}`}>View Record</Link>
+                                            <Link to={`/admin-view-user/${user.id}`}>View Record <FontAwesomeIcon icon={faArrowRight}></FontAwesomeIcon></Link>
                                             
                                         </div>
                                     </li>
